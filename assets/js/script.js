f40aa378f7f19368a4020a8937c29292daf0ecd5
Angelo Ilardi <angeloilardi@Angelos-MacBook-Air.local>
2023-02-03 16:40:17 +0000

    added error 404 handling and no results message

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,54 @@
-function artistSearch (){
+function artistSearch (artist){
     $.ajax({
-        url: `https://rest.bandsintown.com/artists/beyonce/events?app_id=codingbootcamp`,
-        method: "GET"
+        url: `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`,
+        method: "GET",
+        //error 404 handling
+        error: function (xhr, thrownError){
+            $(".eventList").empty();
+            if (xhr.status == 404) {
+                $(".eventList").html(`<h3 class="mx-auto">Ermm, we couldn't find that. Wanna give it another try?</h3>`).addClass("row align-items-center")
+            }
+        }
     }
     
     ).then(function (response) {
-        console.log(response);
-        let numOfEvents = response.length;
-        console.log(numOfEvents);
-        let currentlyDisplayed = 0
-        if (numOfEvents === 0) {
-            return //add content for no results found
+        // commenting the next few lines as they're not in use right now
+        // let numOfEvents = response.length;
+        //console.log(numOfEvents);
+        // let currentlyDisplayed = 0
+        // message if the are no results
+        if (response.length === 0) {
+            $(".eventList").html(`<h3 class="mx-auto">Sorry, no results for this artist :/</h3>`).addClass("row align-items-center");
+            //calls function to display results
         } else { showEvents (response, 0)   
-        }
-    //     if (numOfEvents > 10){
-    // let showMoreButton = $("<button>").text("Show more")
-    
-    //     }
-        
-    });
+        }       
+    })
 }
     
 
 function showEvents (response, counter) {
+    $(".eventList").empty();
+    // for each event found
     for (let i = counter; i < response.length; i++){
-        let date = response[i].datetime;
-        let rightDate = moment(date, "YYYY-MM-DD").format("DD/MM/YYYY")
+        // gets the date and changes the format
+        let date = moment((response[i].datetime), "YYYY-MM-DD").format("DD/MM/YYYY");
+        // gets the city
         let city = response[i].venue.city;
+        // gets the country
         let country = response[i].venue.country;
+        // gets the name of the venue
         let venue = response[i].venue.name;
-        let eventRow = $("<div>").addClass("row align-items-center")
-        let eventContent = $("<h5>").text(`${rightDate} - ${city} - ${country}`);
-        eventContent.addClass()
+        //creates a row for the event
+        let eventRow = $("<div>").addClass("row align-items-center");
+        // builds the event text
+        let eventContent = $("<h5>").text(`${date} - ${city} - ${country}`);
+        // the venue name with a smaller font
         let eventVenue = $("<h6>").text(`${venue}`).addClass("p-2");
-        let getTicketsBtn = $("<button>").addClass("ml-auto").append((`<a target="_blank" href = ${response[i].url}>Get Tickets</a>`))
-        $(eventRow).append(eventContent, eventVenue, getTicketsBtn)
+        // gets link to the tickets
+        let getTicketsBtn = $("<button>").addClass("ml-auto").append((`<a target="_blank" href = ${response[i].url}>Get Tickets</a>`));
+        $(eventRow).append(eventContent, eventVenue, getTicketsBtn);
         $(".eventList").append(eventRow);
         
     }
 }
-
-artistSearch()
+artistSearch ("beyonce")
